24090da3fd537ab80f187c83f4e16063ddaf598c
Maduka <madukae@8c8590166b9a.ant.amazon.com>
2018-11-07 00:49:46 -0500

    Create and set up bucket

--- 01-webotron/ipythonsession.py
+++ 01-webotron/ipythonsession.py
@@ -1,23 +1,77 @@
 # coding: utf-8
+
+# import s3 session
 import boto3
-get_ipython().run_line_magic('cat', '~/ .aws/config')
-get_ipython().run_line_magic('cat', '~/.aws/config')
-get_ipython().run_line_magic('cat', '~/.aws/config')
-get_ipython().run_line_magic('cat', '~/.aws/config')
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket)
-    
-get_ipython().run_line_magic('pwd', '')
-aws s3 mb s3://automatingawsmadux-cli
-for bucket in s3.buckets.all():
-    print(bucket)
-    
-get_ipython().run_line_magic('history', '')
-s3.create_bucket(Bucket='automatingawsmadux-boto3', CreateBucketConfiguration={'LocationConstraint': us-east-2})
-new_bucket = s3.create_bucket(Bucket='automatingawsmadux-boto3', CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
-for bucket in s3.buckets.all():
-    print(bucket)
-    
-get_ipython().run_line_magic('history', '')
+
+# Create bucket
+new_bucket = s3.create_bucket(Bucket='automatingawsmadux-newbucket', CreateBucketConfiguration={'LocationConstraint': session.region_name})
+
+# Upload index.html file
+new_bucket.upload_file('index.html', 'index.html', ExtraArgs={'ContentType': 'text/html'})  
+
+# Set the bucket policy
+# First set the policy document 
+policy = """  
+    { 
+     "Version":"2012-10-17", 
+     "Statement":[{ 
+     "Sid":"PublicReadGetObject", 
+     "Effect":"Allow", 
+     "Principal": "*", 
+          "Action":["s3:GetObject"], 
+          "Resource":["arn:aws:s3:::%s/*" 
+          ] 
+        } 
+      ] 
+    } 
+    """ % new_bucket.name
+
+# Then use the policy document to set the bucket policy
+
+   pol = new_bucket.Policy()
+   
+   policy = policy.strip() 
+
+   pol.put(Policy=policy)
+   
+   
+# Set up website configuration on bucket
+
+ws = new_bucket.Website()
+
+ws.put(WebsiteConfiguration={
+   			'ErrorDocument': {
+   				'Key': 'error.html'
+   				},
+   				'IndexDocument': {
+   					'Suffix': 'index.html'
+   					}})
+
+# Get website address
+url = "http://%s.s3-website.us-east-2.amazonaws.com"% new_bucket.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
